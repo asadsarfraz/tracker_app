796364ac4ae92075ad49144807c996cab8cb99e7
asadsarfraz <asad.sarfraz@devden.io>
2021-08-16 09:20:29 +0500

    fixed fs events

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
 
+const { IgnorePlugin } = require('webpack');
+
+const optionalPlugins = [];
+if (process.platform !== "darwin") {
+  optionalPlugins.push(new IgnorePlugin({ resourceRegExp: /^fsevents$/ }));
+}
+
+module.exports = {
+  // other webpack config options here...
+  plugins: [
+    ...optionalPlugins,
+  ],
+};
+
 module.exports = environment
