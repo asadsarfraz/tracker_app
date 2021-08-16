b9d8c89ffd74ffd2f54e4361ff3801ac6948722e
asadsarfraz <asad.sarfraz@devden.io>
2021-08-16 10:39:43 +0500

    bootstrap added

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
-const { IgnorePlugin } = require('webpack');
+const webpack = require("webpack")
 
-const optionalPlugins = [];
-if (process.platform !== "darwin") {
-  optionalPlugins.push(new IgnorePlugin({ resourceRegExp: /^fsevents$/ }));
-}
-
-module.exports = {
-  // other webpack config options here...
-  plugins: [
-    ...optionalPlugins,
-  ],
-};
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+  }))
 
 module.exports = environment
