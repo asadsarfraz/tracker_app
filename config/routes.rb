26a2b891620197857fae2ed5a913073ee33143ce
asadsarfraz <asad.sarfraz@devden.io>
2021-08-17 12:23:25 +0500

    all work for showing stocks on browswer and validations

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/welcome", to: "welcome#index"
   root "welcome#index"
   get "/portfolio", to: "users#my_portfolio"
+  get "search_stock", to: "stocks#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
