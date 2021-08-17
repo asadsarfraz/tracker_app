7d0f3369ef841bc645eb42db490a552c00330ea9
asadsarfraz <asad.sarfraz@devden.io>
2021-08-17 10:19:57 +0500

    search icon and bar done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/welcome', to: 'welcome#index'
-  root 'welcome#index'
-  
+  get "/welcome", to: "welcome#index"
+  root "welcome#index"
+  get "/portfolio", to: "users#my_portfolio"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
